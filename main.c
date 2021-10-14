eb4235f8456b4318535d15a8524dbe04cf9d854c
Aaron Hsu <aaronhsu4606@gmail.com>
2021-10-14 18:50:22 -0400

    struct

--- main.c
+++ main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct product {char[] name; double price;};
+struct product {char *name; int quantity;};
+
+void print_product(struct product *p) {
+  printf("%d %s in stock\n", p -> quantity, p -> name);
+}
+
+struct product * make_product(char *n, int q) {
+  struct product *p = malloc(sizeof(struct product));
+
+  p -> name = n;
+  p -> quantity = q;
+
+  return p;
+}
 
 int main() {
 
-    return 0;
+  struct product *p = make_product("potato", 5);
+  print_product(p);
+
+  free(p);
+  return 0;
 }
